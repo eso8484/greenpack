3c3ac5c9226da7b61a9601e30a86f3c25ac22137
Enejo Sunday Oche <enejosundayoche@gmail.com>
2026-02-20 00:33:10 +0100

    just now

--- src/components/layout/SearchBar.tsx
+++ src/components/layout/SearchBar.tsx
@@ -26,7 +26,7 @@ export default function SearchBar({
     <form onSubmit={handleSubmit} className={className}>
       <div className="relative">
         <svg
-          className="absolute left-3 top-1/2 -translate-y-1/2 w-4.5 h-4.5 text-gray-400 dark:text-gray-500"
+          className="absolute left-3 top-1/2 -translate-y-1/2 w-5 h-5 text-gray-400 dark:text-gray-500"
           fill="none"
           stroke="currentColor"
           viewBox="0 0 24 24"
